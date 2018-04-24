7da883d03b8886f721bf0742eb2cbd1b7e114ae8
Jason Mittertreiner <jmittert@uwaterloo.ca>
2018-04-24 13:15:03 -0700

    Remove .dirlocals

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (intero-targets "jcc:lib" "jcc:exe:jcc" "jcc:test:jcc-test")))
-
